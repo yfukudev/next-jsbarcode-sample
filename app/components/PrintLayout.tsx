c9c316e7f011ab160fa8c9965aa6d917c6bc332a
yfukudev <fortune.snail@gmail.com>
2024-06-24 10:16:54 +0900

    fix indent

--- app/components/PrintLayout.tsx
+++ app/components/PrintLayout.tsx
@@ -4,28 +4,25 @@ import React from 'react';
 import Barcode from 'react-jsbarcode';
 
 type PrintLayoutProps = {
-    barcodeValue: string;
+  barcodeValue: string;
 };
 
 export const PrintLayout = React.forwardRef<HTMLDivElement, PrintLayoutProps>((props, ref) => {
-    return (
-        <div ref={ref} className="print-only flex-col item-center h-screen">
-            <h1 className='text-xs text-center mt-5'>Barcode Sample</h1>
-            {props.barcodeValue && (
-                <div className='text-center'>
-                    <Barcode
-                        value={props.barcodeValue}
-                        options={{
-                            width: 1,
-                            height: 20,
-                            fontSize: 10,
-                        }}
-                    />
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div ref={ref} className="print-only flex flex-col items-center h-screen">
+      <h1 className='text-xs text-center mt-5'>Barcode Sample</h1>
+      {props.barcodeValue && (
+        <Barcode
+          value={props.barcodeValue}
+          options={{
+            width: 1,
+            height: 20,
+            fontSize: 10,
+          }}
+        />
+      )}
+    </div>
+  );
 });
 
 PrintLayout.displayName = 'PrintLayout';
-
