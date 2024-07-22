c0a8de21a65fa9625ac766525a3b1d9c444fb5fd
yfukudev <fortune.snail@gmail.com>
2024-07-22 13:45:50 +0900

    fix layout

--- app/components/PrintLayout.tsx
+++ app/components/PrintLayout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image';
 import React from 'react';
 import Barcode from 'react-jsbarcode';
 
@@ -10,7 +11,7 @@ type PrintLayoutProps = {
 export const PrintLayout = React.forwardRef<HTMLDivElement, PrintLayoutProps>(function PrintLayout(props, ref) {
   return (
     <div ref={ref} className="print-only flex flex-col items-center h-screen">
-      <h1 className='text-xs text-center mt-5'>Barcode Sample</h1>
+      <h1 className='text-xs text-center mt-5'>First Page</h1>
       {props.barcodeValue && (
         <Barcode
           value={props.barcodeValue}
@@ -21,6 +22,9 @@ export const PrintLayout = React.forwardRef<HTMLDivElement, PrintLayoutProps>(fu
           }}
         />
       )}
+      <div className="next-page"></div>
+      <h1 className='text-xs text-center mt-5'>Second Page</h1>
+      <Image src="https://placehold.jp/100x50.png" width={100} height={50} alt='sample image' />
     </div>
   );
 });
